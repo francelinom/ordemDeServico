13d5a28ef8681b11a4e2cbcc24c4094fabcc53f8
Francelino <francelino.silva.prestserv@petrobras.com.br>
2023-04-18 23:00:02 -0300

    Classe Tecnico

---
+++ src/main/java/br/com/devfrancelino/ordemdeservico/domain/Tecnico.java
@@ -0,0 +1,12 @@
+package br.com.devfrancelino.ordemdeservico.domain;
+
+public class Tecnico extends Pessoa{
+
+    public Tecnico() {
+        super();
+    }
+
+    public Tecnico(Integer id, String nome, String cpf, String telefone) {
+        super();
+    }
+}
