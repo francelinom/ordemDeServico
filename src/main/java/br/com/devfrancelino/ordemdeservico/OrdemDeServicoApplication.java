9143370111df11b7b83ac670054e7e16aac72e5d
Francelino <francelino.silva.prestserv@petrobras.com.br>
2023-04-18 22:40:21 -0300

    first commit

---
+++ src/main/java/br/com/devfrancelino/ordemdeservico/OrdemDeServicoApplication.java
@@ -0,0 +1,13 @@
+package br.com.devfrancelino.ordemdeservico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdemDeServicoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdemDeServicoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/devfrancelino/ordemdeservico/OrdemDeServicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devfrancelino.ordemdeservico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdemDeServicoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
