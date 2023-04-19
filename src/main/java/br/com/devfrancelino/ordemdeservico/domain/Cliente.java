6568713d4e023e0bef1c243f35995b3c1e9d10c7
Francelino <francelino.silva.prestserv@petrobras.com.br>
2023-04-18 22:59:51 -0300

    Classe Cliente

---
+++ src/main/java/br/com/devfrancelino/ordemdeservico/domain/Cliente.java
@@ -0,0 +1,12 @@
+package br.com.devfrancelino.ordemdeservico.domain;
+
+public class Cliente extends Pessoa{
+
+    public Cliente() {
+        super();
+    }
+
+    public Cliente(Integer id, String nome, String cpf, String telefone) {
+        super();
+    }
+}
