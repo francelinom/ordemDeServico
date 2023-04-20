4d82fd3ff9aee971a602fdbeff4867d56c80897c
Francelino <francelino.silva.prestserv@petrobras.com.br>
2023-04-20 15:51:48 -0300

    - feat adicionando a anotação @Entity - feat adicionando o @Id e estrategia de geração de id - feat adicionando o validador de CPF

--- src/main/java/br/com/devfrancelino/ordemdeservico/domain/Pessoa.java
+++ src/main/java/br/com/devfrancelino/ordemdeservico/domain/Pessoa.java
@@ -1,12 +1,22 @@
 package br.com.devfrancelino.ordemdeservico.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
+import org.hibernate.validator.constraints.br.CPF;
 
 @Data
+@Entity
 public abstract class Pessoa {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
+
+    @CPF
     private String cpf;
     private String telefone;
 
