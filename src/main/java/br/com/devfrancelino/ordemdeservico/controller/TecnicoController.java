df50c951bb2ca6a5a7900778101b835abba9b8f6
Francelino <francelino.silva.prestserv@petrobras.com.br>
2023-04-26 14:46:19 -0300

    - Feat endpoint de busca tecnico por ID

---
+++ src/main/java/br/com/devfrancelino/ordemdeservico/controller/TecnicoController.java
@@ -0,0 +1,25 @@
+package br.com.devfrancelino.ordemdeservico.controller;
+
+import br.com.devfrancelino.ordemdeservico.domain.Tecnico;
+import br.com.devfrancelino.ordemdeservico.services.TecnicoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/tecnicos")
+public class TecnicoController {
+
+    @Autowired
+    private TecnicoService tecnicoService;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Tecnico> findById (@PathVariable Integer id) {
+        Tecnico obj = tecnicoService.findById(id);
+
+        return ResponseEntity.ok().body(obj);
+    }
+}
