ae567ab0dc27e1d13cbc08fb9a4de16a1d97a0a2
Francelino <francelino.silva.prestserv@petrobras.com.br>
2023-05-07 22:29:34 -0300

    - Feat DTO para ordem de serviço

---
+++ src/main/java/br/com/devfrancelino/ordemdeservico/dto/OsDTO.java
@@ -0,0 +1,46 @@
+package br.com.devfrancelino.ordemdeservico.dto;
+
+import br.com.devfrancelino.ordemdeservico.domain.Cliente;
+import br.com.devfrancelino.ordemdeservico.domain.OS;
+import br.com.devfrancelino.ordemdeservico.domain.Tecnico;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class OsDTO implements Serializable {
+    private static final Long serialVersionUID = 1L;
+
+    private Integer id;
+
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+    private LocalDateTime dataAbertura;
+
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+    private LocalDateTime dataFechamento;
+    private Integer prioridade;
+    private String observacoes;
+    private Integer status;
+    private Integer tecnico;
+    private Integer cliente;
+
+    public OsDTO() {
+        super();
+    }
+
+    public OsDTO(OS os) {
+        super();
+        this.id = os.getId();
+        this.dataAbertura = os.getDataAbertura();
+        this.dataFechamento = os.getDataFechamento();
+        this.prioridade = os.getPriodidade().getCod();
+        this.observacoes = os.getObservacoes();
+        this.status = os.getStatus().getCod();
+        this.tecnico = os.getTecnico().getId();
+        this.cliente = os.getCliente().getId();
+    }
+}
