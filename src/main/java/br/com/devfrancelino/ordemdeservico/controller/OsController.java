d0251fb778fd57ff084267c6760c9beeaccac5e8
Francelino <francelino.silva.prestserv@petrobras.com.br>
2023-05-07 22:36:38 -0300

    - Feat endpoint para consultar todas as ordens de serviços

--- src/main/java/br/com/devfrancelino/ordemdeservico/controller/OsController.java
+++ src/main/java/br/com/devfrancelino/ordemdeservico/controller/OsController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping(value = "/os")
 public class OsController {
@@ -20,6 +23,13 @@ public class OsController {
     public ResponseEntity<OsDTO> findById(@PathVariable Integer id) {
         OsDTO osDTO = new OsDTO(osService.findById(id));
         return ResponseEntity.ok().body(osDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OsDTO>> findAll() {
+        List<OsDTO> osDTOList = osService.findAll().stream().map(obj -> new OsDTO(obj)).collect(Collectors.toList());
+
+        return ResponseEntity.ok().body(osDTOList);
     }
 
 }
--- src/main/java/br/com/devfrancelino/ordemdeservico/services/OsService.java
+++ src/main/java/br/com/devfrancelino/ordemdeservico/services/OsService.java
@@ -1,11 +1,13 @@
 package br.com.devfrancelino.ordemdeservico.services;
 
 import br.com.devfrancelino.ordemdeservico.domain.OS;
+import br.com.devfrancelino.ordemdeservico.dto.OsDTO;
 import br.com.devfrancelino.ordemdeservico.repositories.OSRepository;
 import br.com.devfrancelino.ordemdeservico.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,5 +19,9 @@ public class OsService {
     public OS findById(Integer id) {
         Optional<OS> obj = osRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + " Tipo: " + OS.class.getName()));
+    }
+
+    public List<OS> findAll() {
+        return osRepository.findAll();
     }
 }
