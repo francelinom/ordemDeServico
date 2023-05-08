f311416ff98c71d91c83d23f8d4b5d14a77795eb
Francelino <francelino.silva.prestserv@petrobras.com.br>
2023-05-07 22:56:30 -0300

    - Feat endpoint para criar nova ordem de serviço

--- src/main/java/br/com/devfrancelino/ordemdeservico/controller/OsController.java
+++ src/main/java/br/com/devfrancelino/ordemdeservico/controller/OsController.java
@@ -2,13 +2,13 @@ package br.com.devfrancelino.ordemdeservico.controller;
 
 import br.com.devfrancelino.ordemdeservico.dto.OsDTO;
 import br.com.devfrancelino.ordemdeservico.services.OsService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,6 +30,14 @@ public class OsController {
         List<OsDTO> osDTOList = osService.findAll().stream().map(obj -> new OsDTO(obj)).collect(Collectors.toList());
 
         return ResponseEntity.ok().body(osDTOList);
+    }
+
+    @PostMapping
+    public ResponseEntity<OsDTO> create(@Valid @RequestBody OsDTO osDTO) {
+        osDTO = new OsDTO(osService.create(osDTO));
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(osDTO.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+
     }
 
 }
--- src/main/java/br/com/devfrancelino/ordemdeservico/services/OsService.java
+++ src/main/java/br/com/devfrancelino/ordemdeservico/services/OsService.java
@@ -1,6 +1,10 @@
 package br.com.devfrancelino.ordemdeservico.services;
 
+import br.com.devfrancelino.ordemdeservico.domain.Cliente;
 import br.com.devfrancelino.ordemdeservico.domain.OS;
+import br.com.devfrancelino.ordemdeservico.domain.Tecnico;
+import br.com.devfrancelino.ordemdeservico.domain.enums.Priodidade;
+import br.com.devfrancelino.ordemdeservico.domain.enums.Status;
 import br.com.devfrancelino.ordemdeservico.dto.OsDTO;
 import br.com.devfrancelino.ordemdeservico.repositories.OSRepository;
 import br.com.devfrancelino.ordemdeservico.services.exceptions.ObjectNotFoundException;
@@ -16,6 +20,12 @@ public class OsService {
     @Autowired
     private OSRepository osRepository;
 
+    @Autowired
+    private TecnicoService tecnicoService;
+
+    @Autowired
+    private ClienteService clienteService;
+
     public OS findById(Integer id) {
         Optional<OS> obj = osRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + " Tipo: " + OS.class.getName()));
@@ -23,5 +33,25 @@ public class OsService {
 
     public List<OS> findAll() {
         return osRepository.findAll();
+    }
+
+    public OS create(OsDTO osDTO) {
+        return fromDTO(osDTO);
+    }
+
+    private OS fromDTO(OsDTO osDTO) {
+
+        Tecnico tecnico = tecnicoService.findById(osDTO.getTecnico());
+        Cliente cliente = clienteService.findById(osDTO.getCliente());
+
+        OS newOs = new OS();
+        newOs.setId(osDTO.getId());
+        newOs.setObservacoes(osDTO.getObservacoes());
+        newOs.setPriodidade(Priodidade.toEnum(osDTO.getPrioridade()));
+        newOs.setStatus(Status.toEnum(osDTO.getStatus()));
+        newOs.setTecnico(tecnico);
+        newOs.setCliente(cliente);
+
+        return osRepository.save(newOs);
     }
 }
