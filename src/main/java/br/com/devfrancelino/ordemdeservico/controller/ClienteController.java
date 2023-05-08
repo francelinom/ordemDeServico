46d7508e53060c63623754e98b9a1bd644729d7b
Francelino <francelino.silva.prestserv@petrobras.com.br>
2023-05-07 21:40:56 -0300

    - Feat endpoint para criar cliente

--- src/main/java/br/com/devfrancelino/ordemdeservico/controller/ClienteController.java
+++ src/main/java/br/com/devfrancelino/ordemdeservico/controller/ClienteController.java
@@ -3,13 +3,13 @@ package br.com.devfrancelino.ordemdeservico.controller;
 import br.com.devfrancelino.ordemdeservico.domain.Cliente;
 import br.com.devfrancelino.ordemdeservico.dto.ClienteDTO;
 import br.com.devfrancelino.ordemdeservico.services.ClienteService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -35,5 +35,12 @@ public class ClienteController {
                 .map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
 
         return ResponseEntity.ok().body(clienteDTOList);
+    }
+
+    @PostMapping
+    public ResponseEntity<ClienteDTO> create(@Valid @RequestBody ClienteDTO clienteDTO) {
+        Cliente cliente = clienteService.create(clienteDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/id").buildAndExpand(cliente.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/br/com/devfrancelino/ordemdeservico/services/ClienteService.java
+++ src/main/java/br/com/devfrancelino/ordemdeservico/services/ClienteService.java
@@ -1,8 +1,11 @@
 package br.com.devfrancelino.ordemdeservico.services;
 
 import br.com.devfrancelino.ordemdeservico.domain.Cliente;
+import br.com.devfrancelino.ordemdeservico.domain.Pessoa;
+import br.com.devfrancelino.ordemdeservico.dto.ClienteDTO;
 import br.com.devfrancelino.ordemdeservico.repositories.ClienteRepository;
 import br.com.devfrancelino.ordemdeservico.repositories.PessoaRepository;
+import br.com.devfrancelino.ordemdeservico.services.exceptions.DataIntegratyViolationException;
 import br.com.devfrancelino.ordemdeservico.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -28,5 +31,22 @@ public class ClienteService {
 
     public List<Cliente> findAll() {
         return clienteRepository.findAll();
+    }
+
+    public Cliente create(ClienteDTO clienteDTO) {
+        if (findCPF(clienteDTO) != null) {
+            throw new DataIntegratyViolationException("CPF já cadastrado na base de dados!");
+        }
+        return clienteRepository.save(new Cliente(null, clienteDTO.getNome(), clienteDTO.getCpf(), clienteDTO.getTelefone()));
+    }
+
+    private Pessoa findCPF(ClienteDTO clienteDTO) {
+        Pessoa pessoa = pessoaRepository.findByCPF(clienteDTO.getCpf());
+
+        if (pessoa != null) {
+            return pessoa;
+        }
+
+        return null;
     }
 }
